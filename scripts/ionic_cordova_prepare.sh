365514bb9832d2cc1b5faf5c0a0c2817629972bc
ikarus512 <ikarus512@yandex.ru>
2017-08-02 14:52:40 +0300

    cosmetic

--- scripts/ionic_cordova_prepare.sh
+++ scripts/ionic_cordova_prepare.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+chmod +x hooks/after_prepare/010_add_platform_class.js
+
 if [ ! -e platforms ];then
 
-    chmod +x hooks/after_prepare/010_add_platform_class.js
     ionic cordova prepare #--verbose
 
 fi
