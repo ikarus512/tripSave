9758c73a1a37400516c42db953593c44c11efaf0
ikarus512 <ikarus512@yandex.ru>
2017-08-03 11:55:02 +0300

    release build added

---
+++ scripts/ionic_cordova_build_android_release.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+#https://unix.stackexchange.com/questions/122460/bash-how-to-let-some-background-processes-run-but-wait-for-others
+
+if [ -z $zipalign ];then zipalign=$ANDROID_HOME1/build-tools/25.0.2/zipalign; fi
+
+### Release build:
+# ionic cordova plugin rm cordova-plugin-console
+# ionic cordova build android --release -- --keystore=$keystoreFile --alias=$keystoreAlias --storePassword=$storePassword --password=$keyPassword
+ionic cordova build android --release || exit 1
+
+pushd platforms/android/build/outputs/apk
+
+    ### keystore:    CN=Your name, OU=OrgUnit, O=Org, L=city/Locality, S=STate/province, C=Country code
+    keystoreFile=ikarus512-trackSave.keystore
+    keystoreAlias=ikarus512trackSave
+    storePassword=$(dd if=/dev/urandom bs=32 count=1 2>/dev/null | sha256sum -b | sed 's/ .*//')
+    keyPassword=$(dd if=/dev/urandom bs=32 count=1 2>/dev/null | sha256sum -b | sed 's/ .*//')
+    keystoreValidity=10000 # days
+
+    rm -f $keystoreFile || exit 1
+    keytool -genkey -v -noprompt -alias $keystoreAlias -keystore $keystoreFile -storepass $storePassword -keypass $keyPassword -keyalg RSA -keysize 2048 -validity $keystoreValidity -dname "CN=ikarus512, OU=ikarus512, O=HSH, L=NN, S=RU, C=RU" || exit 1
+
+    #android-armv7-release-unsigned.apk
+    #android-x86-release-unsigned.apk
+    for apkFile in $(ls android-*-release-unsigned.apk);do
+
+        plat=$(echo $apkFile | perl -e '$_=<>; s/^\w+-|-release-unsigned.apk$//g; print;')
+        apkFileOut=trackSave-$plat.apk
+        rm -f $apkFileOut || exit 1
+
+        jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $keystoreFile $apkFile $keystoreAlias -storepass $storePassword -keypass $keyPassword || exit 1
+        jarsigner -verify $apkFile $keystoreAlias || exit 1
+
+        $zipalign -v 4 $apkFile $apkFileOut || exit 1
+
+    done
+
+popd
