bb771129a81175d41d305c6977f3ce6b626654ab
ikarus512 <ikarus512@yandex.ru>
2017-08-03 22:22:51 +0300

    GitHub releases deployment

--- scripts/ionic_cordova_build_android_debug.sh
+++ scripts/ionic_cordova_build_android_debug.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+myappname=tripSave
+
 ionic cordova build android || exit 1 # --verbose
+
+pushd platforms/android/build/outputs/apk || exit 1
+    cp -frv android-armv7-debug.apk $myappname-debug.apk
+    cp -frv android-x86-debug.apk   $myappname-debug-x86.apk
+popd
