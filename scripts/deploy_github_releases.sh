223674c68f9ecb80a935ff5d003ea2db797b4b68
ikarus512 <ikarus512@yandex.ru>
2017-08-04 09:57:34 +0300

    deploy experiments: tag

--- scripts/deploy_github_releases.sh
+++ scripts/deploy_github_releases.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-git push --tags
+### Create a git tag of the new version to use
+### http://phdesign.com.au/programming/auto-increment-project-version-from-travis
+# If package.json major and minor versions match last tag, then increment last tag. Else use package.json major.minor.0.
+"{ sed -nE 's/^[ \\t]*\"version\": \"([0-9]{1,}\\.[0-9]{1,}\\.)[0-9x]{1,}\",$/\\1/p' package.json; git describe --abbrev=0 | sed -E 's/^v([0-9]{1,}\\.[0-9]{1,}\\.)([0-9]{1,})$/\\1 \\2/g'; } | tr \"\\n\" \" \" | awk '{printf($1==$2?\"v\"$2$3+1:\"v\"$1\"0\")}' | xargs -I {} git tag -a {} -m \"{}\"\n"
+
+# Update package.json based on the git tag we just created
+npm --no-git-tag-version version from-git
+
+git status
+git diff -w
+
+#git push --tags || exit 1
