6c2b56da9e927ceeae0cbe95ee59b8063f15e4d2
ikarus512 <ikarus512@yandex.ru>
2017-08-07 21:20:05 +0300

    build fix

--- scripts/android_sdk_install.sh
+++ scripts/android_sdk_install.sh
@@ -12,7 +12,7 @@ if [ ! -e android-sdk-linux/tools/bin ];then
     #android list sdk --extended # && android list sdk -a --extended
 
     ANDROPACKS=tools,platform-tools,build-tools-26.0.1,android-16,android-26
-    ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --silent -u -a -f -t ${ANDROPACKS}    >/dev/null
+    ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk -u -a -f -t ${ANDROPACKS}    >/dev/null 2>&1
 
     ### Accept licenses:
     # - mkdir $ANDROID_HOME/licenses
@@ -20,8 +20,8 @@ if [ ! -e android-sdk-linux/tools/bin ];then
     # - echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
 
     # That requests us to accept a license for the sdkmanager, and then
-    echo y | sdkmanager --update    >/dev/null
+    echo y | sdkmanager --update    >/dev/null 2>&1
     # requests us to accept new licenses not previously accepted
-    ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | sdkmanager --licenses    >/dev/null
+    ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | sdkmanager --licenses    >/dev/null 2>&1
 
 fi
